0d0af14fffcb377076d5a80f7d6b4543074e5399
gjsgabriel <gjsgabriel83@gmail.com>
2022-09-04 16:32:20 -0300

    findByID implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,14 +14,14 @@ public class Program {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "bob","bob@gmail.com",sdf.parse("04/09/2022"), 3000.00, obj);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		
-		System.out.println(obj);
+		Seller seller = sellerDao.findByID(3);
 		
 		System.out.println(seller);
+		
+	
 	}
 
 }
