dac016876afb59aaa5ebcbe2ee923ca6571bc8d4
gjsgabriel <gjsgabriel83@gmail.com>
2022-09-05 12:55:49 -0300

    FindByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -17,12 +18,16 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		
+		System.out.println("=== teste1: seller findById =====");
 		Seller seller = sellerDao.findByID(3);
-		
 		System.out.println(seller);
 		
-	
+		System.out.println("\n=== teste2: seller findByDepartment =====");
+		Department department = new Department(2,null);
+		List<Seller> list =  sellerDao.findByDepartment(department);
+		for(Seller obj : list ) {
+			System.out.println(obj);
+		}
 	}
 
 }
